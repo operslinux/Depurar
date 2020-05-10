840c6707ccfc55d4397fd2762701a98a1f65e280
señor xxx <xxx@localhost.localdomain>
2020-05-10 14:53:06 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,48 @@
+```
+___  ___  __ __ __      __  _____    __     __  __
+  /___\/ _ \/__/__/ _\    / /  \_   \/\ \ \/\ /\ \/ /
+ //  // /_)/_\/ \/\ \    / /    / /\/  \/ / / \ \  / 
+/ \_// ___//_/ _  _\ \  / /__/\/ /_/ /\  /\ \_/ /  \ 
+\___/\/   \__\/ \_\__/  \____\____/\_\ \/  \___/_/\_\
+
+
+                               (0 0) 
+                       ---oOO-- (_) ----oOO---    
+                     ╔═════════════════════════╗ 
+                     ║ Bienvenido by OPERS!..  ║ 
+                     ╚═════════════════════════╝ 
+                        -------------------
+                              |__|__| 
+                               || || 
+                              ooO Ooo 
+```
+```
+Este es un pequeño script muy sencillo para poder automatizar y depurar los correos con formato especial, como el del ejemplo div-ejemplo.txt
+```
+
+<a href="https://ibb.co/Dgqvs7d"><img src="https://i.ibb.co/LhGcbCK/depurar.png" alt="depurar" border="0"></a>
+
+# [Instalacion]
+```
+$ apt update && apt upgrade
+```
+```
+$ apt install git python3
+```
+```
+$ git clone https://github.com/operslinux/Depurar.git
+```
+```
+$ cd Depurar
+```
+
+# [ Setup ]
+```
+$ pip3 install -r requirements.txt
+```
+# [ Running ]
+```
+$ python3 run.py
+```
+
+[Canal:] https://www.youtube.com/opersweenlinux
---
+++ run.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+from form.logo import LogoOne, LogoCero, LogoTwo
+from colorama import Back, Fore, init
+from tqdm import tqdm
+import os
+#iniciamos colores
+from time import sleep
+init()
+
+
+def Carga():
+    loop = tqdm(total=50000, position=0, leave=False)
+    for k in range(50000):
+        loop.set_description(Fore.BLUE + "Loading .....".format(k) + Fore.RESET)
+        loop.update(1)
+    loop.close()
+
+
+def MenuInicial():
+    os.system('clear')
+    LogoCero()
+    sleep(0.7)
+    os.system('clear')
+    sleep(0.3)
+    LogoCero()
+    sleep(0.3)
+    os.system('clear')
+    sleep(0.3)
+    LogoOne()
+    sleep(0.3)
+    os.system('clear')
+    sleep(0.3)
+    LogoCero()
+    sleep(1.5)
+    sleep(0.60)
+    Carga()
+    print(Fore.GREEN + "\t\tBienvenido a Check Mail BY OPERS LINUX\t\t")
+    print("\t\t__________________________\t\t")
+
+
+def main():
+    MenuInicial()
+    try:
+        txt = open("div.txt", "r")
+        for i, linea in enumerate(txt):
+
+            #print("La Linea seleccionada es: ", linea)
+            #print("La enumeracion es: ", i)
+            print("{} [ {} + {} ] {} < Correo numero: > {}".format(Fore.BLUE, Fore.GREEN, Fore.BLUE, Fore.YELLOW, Fore.RESET) + str(i))
+            dividir = linea.split(">> ")
+            print(dividir[1])
+            f = open("list.txt", "a")
+            f.write(dividir[1])
+            f.close()
+
+    except NameError:
+        LogoTwo()
+        print("Hay un caracter mal en el archivo")
+    except IOError:
+        LogoTwo()
+        print("Coloque el archivo txt llamado div con nombres y correos")
+    except IndexError:
+        
+        print("La linea no coincide con el acomodo que se requiere, revise el numero de linea en el archivo DIV")
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
